634b30818da4a60a3fff8dfa3bbf6a859c8f6d7b
Mehwishnaz <160154061+Mehwishnaz1603@users.noreply.github.com>
2024-04-04 17:52:40 +0500

    Task no 36

---
+++ index.ts
@@ -0,0 +1,12 @@
+     ///Task # 36 ////
+
+     //Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names 
+//in a array, and then use a for loop to print the name of each pizza.
+//• Modify your for loop to print a sentence using the name of the pizza instead of printing 
+//just the name of the pizza. For each pizza you should have one line of output containing a 
+//simple statement like I like pepperoni pizza
+
+let fvt_pizza: string[] = ["peproni","Cheese", "BBQ Chicken", "Sicilian"]
+for (let pizzas of fvt_pizza) {
+    console.log(`I like ${pizzas} pizza.`)}
+    console.log(`I really love Pizzas`)
